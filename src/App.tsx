7597e5c6f4cc9234b4efe1f2159aff8a1fb9f0ed
Olivier0324 <techies.dev24@gmail.com>
2025-09-10 11:28:18 +0200

    Update: I have added logo in index.html and created basic   routes for admin

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,26 @@ import Signup from './pages/Signup';
 import ForgotPassword from './pages/Forgotpassword';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Help from './pages/Help';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import Overview from './pages/admin/Overview';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/landingpage" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/help" element={<Help />} /> {/* ✅ Help at /help */}
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/landingpage' element={<LandingPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/forgotpassword' element={<ForgotPassword />} /> {/* ✅ new route */}
+        <Route path='/about' element={<About />} /> {/* ✅ new route */}
+        <Route path='/contact' element={<Contact />} /> {/* ✅ new route */}
+        <Route path='admin' element={<AdminDashboard />}>
+          <Route index element={<Overview />} />
+          
+        </Route>
       </Routes>
+     
     </Router>
   );
 };
